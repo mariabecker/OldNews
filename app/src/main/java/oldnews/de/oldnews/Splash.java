c659cd7c8639abfd66a5b42fd8b8849dfeec8d84
Kathrin Konkol <konkol.k@live.de>
2017-11-17 15:24:24 +0100

    app icon, splash screen, fixed images, nice content views

---
+++ app/src/main/java/oldnews/de/oldnews/Splash.java
@@ -0,0 +1,31 @@
+package oldnews.de.oldnews;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
+import android.view.Menu;
+
+public class Splash extends Activity {
+    /** Duration of wait **/
+    private final int SPLASH_DISPLAY_LENGTH = 2000;
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        setContentView(R.layout.splash_layout);
+
+        /* New Handler to start the Menu-Activity
+         * and close this Splash-Screen after some seconds.*/
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(Splash.this,MainActivity.class);
+                Splash.this.startActivity(mainIntent);
+                Splash.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
